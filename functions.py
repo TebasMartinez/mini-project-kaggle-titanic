94a450a07d3c85b18d8f6efbf30ae66d3e954a6d
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-09-23 16:58:16 +0200

    Decision Tree

--- functions.py
+++ functions.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
-def clean_test(df):
+def import_train_data():
+    df = pd.read_csv('data/train1.csv')
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+    X = df.drop(columns=['Survived', 'PassengerId'])
+    y = df['Survived']
+    return df, X, y
+
+def import_clean_test():
+    df = pd.read_csv('src/test.csv')
     df.Age = df.Age.fillna(df.Age.mean())
     df.Fare = df.Fare.fillna(df.Fare.mean())
     embarked_dummies = pd.get_dummies(df['Embarked'], dtype=int)
@@ -9,3 +17,12 @@ def clean_test(df):
     df = pd.concat([df.drop(columns=['Sex']), sex_dummies], axis=1)
     df.drop(columns=['Name', 'Ticket', 'Cabin'], inplace=True)
     return df
+
+def generate_results(model, test_df, output_csv_name):
+    y_pred = model.predict(test_df.drop(columns=['PassengerId']))
+    results = pd.DataFrame({
+    "PassengerId":test_df['PassengerId'],
+    "Survived":y_pred
+    })
+    results.set_index('PassengerId', inplace=True)
+    results.to_csv(f'results/{output_csv_name}')
